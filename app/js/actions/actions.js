4e246c7f62086842d88527d8e5c513a5a929a41d
j4smyn <73558392+j4smyn@users.noreply.github.com>
2020-11-09 00:31:42 +0000

    Create actions.js

---
+++ app/js/actions/actions.js
@@ -0,0 +1,9 @@
+export const API_KEY = '11bc9fe08c1f10b371f6294350503cab';
+export const FETCHING_MOVIES = 'FETCHING_MOVIES';
+export const SET_MOVIES = 'SET_MOVIES';
+export const SET_CURRENT_MOVIE = 'SET_CURRENT_MOVIE';
+export const FETCHING_TV_SHOWS = 'FETCHING_TV_SHOWS';
+export const SET_TV_SHOWS = 'SET_TV_SHOWS';
+export const SET_CURRENT_TV_SHOW = 'SET_CURRENT_TV_SHOW';
+export const FETCHING_DATA = 'FETCHING_DATA';
+export const SET_PAGE = 'SET_PAGE';
